7d507513d18e447408741a1af5b8df6e38e7ff44
pojimoon <kkr2290107@stu.o-hara.ac.jp>
2023-11-13 12:02:48 +0900

    url_holding_table.sqlを追加

--- DB/url_holding_table.sql
+++ DB/url_holding_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2023-11-13 03:17:24
+-- 生成日時: 2023-11-13 04:00:52
 -- サーバのバージョン： 10.4.28-MariaDB
 -- PHP のバージョン: 8.2.4
 
@@ -38,6 +38,26 @@ CREATE TABLE `url_holding` (
   `delete_date` datetime NOT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='ユーザ';
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `url_holding`
+--
+ALTER TABLE `url_holding`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `url_holding`
+--
+ALTER TABLE `url_holding`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
