e30acbf7794e3976db2ccfdc212ffe8927d1dfe1
pojimoon <kkr2290107@stu.o-hara.ac.jp>
2023-11-15 11:26:46 +0900

    url_holding.sqlを追加

---
+++ DB/url_holding.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2023-11-15 03:24:55
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `konchan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `url_holding`
+--
+
+CREATE TABLE `url_holding` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `user_id` bigint(20) NOT NULL COMMENT 'ユーザid',
+  `email` varchar(256) NOT NULL COMMENT 'メールアドレス',
+  `period_available_url` datetime NOT NULL COMMENT 'URL利用可能期間',
+  `password_url` varchar(256) NOT NULL COMMENT 'トークン(URL）',
+  `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
+  `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
+  `delete_date` datetime NOT NULL COMMENT '削除日時',
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='ユーザ';
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `url_holding`
+--
+ALTER TABLE `url_holding`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `user_id_2` (`user_id`),
+  ADD KEY `url_holding_ibfk_2` (`email`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `url_holding`
+--
+ALTER TABLE `url_holding`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `url_holding`
+--
+ALTER TABLE `url_holding`
+  ADD CONSTRAINT `url_holding_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `url_holding_ibfk_2` FOREIGN KEY (`email`) REFERENCES `user` (`email`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
