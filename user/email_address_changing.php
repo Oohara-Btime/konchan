fa496fcd541c9ed0a34bac8824aac8d1bc7c3150
hiyugo <kkr2290054@stu.o-hara.ac.jp>
2023-12-01 13:42:11 +0900

    htmlからphpに変更

---
+++ user/email_address_changing.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="setting.css">
+    </head>
+
+    <body>
+        <h1>メールアドレス変更</h1>
+        <div class="form">
+            <form action="#">
+                <!-- <div class="text-input">
+                    <label for="username">現在のメールアドレス</label>
+                    <input type="text" name="username" id="username" placeholder="" />
+                    <span class="separator"> </span>
+                </div> -->
+
+                <div class="text-input">
+                    <label for="username">新しいメールアドレス</label>
+                    <input type="text" name="username" id="username" placeholder="" />
+                    <span class="separator"> </span>
+                </div>
+
+                <div class="text-input">
+                    <label for="password">メールアドレスの確認</label>
+                    <input type="password" name="password" id="password" placeholder="" />
+                    <span class="separator"> </span>
+                </div>
+
+                <div class="form-bottom">
+                    <input type="submit" id="submit" value="完了" />
+                    <!-- <a href="https://hub.docker.com/login/" class="original-src">Original Source</a> -->
+                </div>
+            </form>
+        </div>
+        <script src="setting.js"></script>
+    </body>
+</html>
---
+++ user/password_resetting.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="setting.css">
+    </head>
+
+    <body>
+        <h1>パスワード再設定</h1>
+        <div class="form">
+            <form action="#">
+                <div class="text-input">
+                    <label for="username">新しいパスワード</label>
+                    <input type="text" name="username" id="username" placeholder="" />
+                    <span class="separator"> </span>
+                </div>
+
+                <div class="text-input">
+                    <label for="password">パスワードの確認</label>
+                    <input type="password" name="password" id="password" placeholder="" />
+                    <span class="separator"> </span>
+                </div>
+
+                <div class="form-bottom">
+                    <input type="submit" id="submit" value="完了" />
+                    <!-- <a href="https://hub.docker.com/login/" class="original-src">Original Source</a> -->
+                </div>
+            </form>
+        </div>
+        <script src="setting.js"></script>
+    </body>
+</html>
