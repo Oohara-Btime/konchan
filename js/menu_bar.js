90ed1f15c70a899a25fc8cc39b8f9c5b62a30b0f
hiyamin <kkr2290007@stu.o-hara.ac.jp>
2023-12-06 15:14:43 +0900

    cssとhtml を　反映

--- js/menu_bar.js
+++ js/menu_bar.js
@@ -1,6 +1,6 @@
 document.querySelector('.burger').addEventListener('click', function (e) {
     e.preventDefault();
-    this.classList.toggle('on')
+    this.classList.toggle('on');
     let checkbox = document.getElementById('drawer_input');
     checkbox.checked = !checkbox.checked;
 });
