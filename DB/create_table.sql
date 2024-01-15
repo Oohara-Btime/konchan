ed9e97a014c80923adfd9140c2261091dfd42c6f
pojimoon <kkr2290107@stu.o-hara.ac.jp>
2024-01-15 11:45:58 +0900

    DBが復旧しましたので更新をお願いします

--- DB/create_table.sql
+++ DB/create_table.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2023-11-15 02:10:02
--- サーバのバージョン： 10.4.28-MariaDB
--- PHP のバージョン: 8.2.4
+-- 生成日時: 2024-01-15 03:14:06
+-- サーバのバージョン： 10.4.32-MariaDB
+-- PHP のバージョン: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
-
 -- データベース: `konchan`
 -- テーブルの構造 `user`
 CREATE TABLE `user` (
@@ -27,9 +26,12 @@ CREATE TABLE `user` (
   `password` varchar(256) NOT NULL COMMENT 'パスワード',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='ユーザ';
+-- テーブルのデータのダンプ `user`
+INSERT INTO `user` (`id`, `email`, `password`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, 'nissy0930@gmail.com', 'nissy0930', '2024-01-11 11:32:38', '2024-01-11 11:32:38', NULL, 0);
 -- テーブルのインデックス `user`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
@@ -37,11 +39,8 @@ ALTER TABLE `user`
   ADD KEY `email` (`email`);
 -- テーブルの AUTO_INCREMENT `user`
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=2;
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `prize`
@@ -50,17 +49,20 @@ CREATE TABLE `prize` (
   `prize_name` varchar(256) NOT NULL COMMENT '景品名',
   `prize_image` blob DEFAULT NULL COMMENT '景品画像'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='景品';
+-- テーブルのデータのダンプ `prize`
+INSERT INTO `prize` (`id`, `prize_name`, `prize_image`) VALUES
+(1, 'A賞', NULL),
+(2, 'B賞', NULL),
+(3, 'C賞', NULL),
+(4, 'D賞', NULL);
 -- テーブルのインデックス `prize`
 ALTER TABLE `prize`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 -- テーブルの AUTO_INCREMENT `prize`
 ALTER TABLE `prize`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=5;
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `m_coupon`
@@ -71,6 +73,12 @@ CREATE TABLE `m_coupon` (
   `discount_rate` int(100) NOT NULL COMMENT '割引率',
   `coupon_image` blob NOT NULL COMMENT 'クーポン画像'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='マスタークーポン';
+-- テーブルのデータのダンプ `m_coupon`
+INSERT INTO `m_coupon` (`id`, `prize_id`, `coupon_name`, `discount_rate`, `coupon_image`) VALUES
+(1, 1, '99%OFFクーポン', 99, ''),
+(2, 2, '98%OFFクーポン', 98, ''),
+(3, 3, '97%OFFクーポン', 97, ''),
+(4, 4, '1%OFFクーポン', 1, '');
 -- テーブルのインデックス `m_coupon`
 ALTER TABLE `m_coupon`
   ADD PRIMARY KEY (`id`),
@@ -79,14 +87,11 @@ ALTER TABLE `m_coupon`
   ADD KEY `id` (`id`);
 -- テーブルの AUTO_INCREMENT `m_coupon`
 ALTER TABLE `m_coupon`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=5;
 -- テーブルの制約 `m_coupon`
 ALTER TABLE `m_coupon`
   ADD CONSTRAINT `m_coupon_ibfk_1` FOREIGN KEY (`prize_id`) REFERENCES `prize` (`id`);
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `url_holding`
@@ -98,7 +103,7 @@ CREATE TABLE `url_holding` (
   `password_url` varchar(256) NOT NULL COMMENT 'トークン(URL）',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='パスワード_URL保持';
 -- テーブルのインデックス `url_holding`
@@ -117,9 +122,6 @@ ALTER TABLE `url_holding`
 COMMIT;
 
 
-
-
-
 -- テーブルの構造 `recipe`
 CREATE TABLE `recipe` (
   `id` bigint(20) NOT NULL COMMENT 'id',
@@ -129,18 +131,21 @@ CREATE TABLE `recipe` (
   `cooking_time` int(255) NOT NULL COMMENT '調理時間',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
-  `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ',
+  `recipe_image` blob NOT NULL COMMENT 'レシピ画像'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='レシピ';
+-- テーブルのデータのダンプ `recipe`
+INSERT INTO `recipe` (`id`, `prefectures_id`, `recipe_name`, `recipe`, `cooking_time`, `create_date`, `update_date`, `delete_date`, `delete_flag`, `recipe_image`) VALUES
+(1, 0, 'アスパラベーコン', '巻いて焼く', 3, '2024-01-11 12:37:08', '2024-01-11 12:37:08', NULL, 0, 0x41422e706e67),
+(2, 0, '切り干し大根', '切って浸す', 9, '2024-01-11 12:37:08', '2024-01-11 12:37:08', NULL, 0, 0x4b442e706e67);
 -- テーブルのインデックス `recipe`
 ALTER TABLE `recipe`
   ADD PRIMARY KEY (`id`);
 -- テーブルの AUTO_INCREMENT `recipe`
 ALTER TABLE `recipe`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=3;
 COMMIT;
-
-
 
 
 -- テーブルの構造 `user_owned_coupon`
@@ -165,10 +170,6 @@ ALTER TABLE `user_owned_coupon`
 COMMIT;
 
 
-
-
-
-
 -- テーブルの構造 `subscription`
 CREATE TABLE `subscription` (
   `email` varchar(256) NOT NULL COMMENT 'メールアドレス',
@@ -176,7 +177,7 @@ CREATE TABLE `subscription` (
   `plan` varchar(256) NOT NULL COMMENT 'プラン		',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時		',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時		',
-  `delete_date` datetime COMMENT '削除日時		',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時		',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ		'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='サブスク';
 -- テーブルのインデックス `subscription`
@@ -188,29 +189,29 @@ ALTER TABLE `subscription`
 COMMIT;
 
 
-
-
-
-
 -- テーブルの構造 `ingredient_category`
 CREATE TABLE `ingredient_category` (
   `id` bigint(20) NOT NULL COMMENT 'id',
   `ingredient_category_name` varchar(256) NOT NULL COMMENT '食材カテゴリ名',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='食材カテゴリ';
+-- テーブルのデータのダンプ `ingredient_category`
+INSERT INTO `ingredient_category` (`id`, `ingredient_category_name`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, '肉', '2024-01-11 11:55:10', '2024-01-11 11:55:10', NULL, 0),
+(2, '魚', '2024-01-11 11:58:22', '2024-01-11 11:58:22', NULL, 0),
+(3, '野菜', '2024-01-11 11:59:05', '2024-01-11 11:59:05', NULL, 0),
+(4, 'その他', '2024-01-11 11:59:50', '2024-01-11 11:59:50', NULL, 0);
 -- テーブルのインデックス `ingredient_category`
 ALTER TABLE `ingredient_category`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 -- テーブルの AUTO_INCREMENT `ingredient_category`
 ALTER TABLE `ingredient_category`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=5;
 COMMIT;
-
-
 
 
 -- テーブルの構造 `foodstuff`
@@ -220,10 +221,15 @@ CREATE TABLE `foodstuff` (
   `ingredient_category_id` bigint(20) NOT NULL COMMENT '食材カテゴリID',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ',
   `ingredient_image` blob NOT NULL COMMENT '食材画像'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='食材';
+-- テーブルのデータのダンプ `foodstuff`
+INSERT INTO `foodstuff` (`id`, `ingredient_name`, `ingredient_category_id`, `create_date`, `update_date`, `delete_date`, `delete_flag`, `ingredient_image`) VALUES
+(1, 'アスパラガス', 3, '2024-01-11 12:20:49', '2024-01-11 12:20:49', NULL, 0, 0x412e706e67),
+(2, 'ベーコン', 1, '2024-01-11 12:20:49', '2024-01-11 12:20:49', NULL, 0, 0x422e706e67),
+(3, '大根', 3, '2024-01-11 12:20:49', '2024-01-11 12:20:49', NULL, 0, 0x442e706e67);
 -- テーブルのインデックス `foodstuff`
 ALTER TABLE `foodstuff`
   ADD PRIMARY KEY (`id`),
@@ -231,14 +237,11 @@ ALTER TABLE `foodstuff`
   ADD KEY `ingredient_category_id` (`ingredient_category_id`);
 -- テーブルの AUTO_INCREMENT `foodstuff`
 ALTER TABLE `foodstuff`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=4;
 -- テーブルの制約 `foodstuff`
 ALTER TABLE `foodstuff`
   ADD CONSTRAINT `foodstuff_ibfk_1` FOREIGN KEY (`ingredient_category_id`) REFERENCES `ingredient_category` (`id`);
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `recipe_ingredient`
@@ -248,9 +251,14 @@ CREATE TABLE `recipe_ingredient` (
   `ingredient_id` bigint(20) NOT NULL COMMENT '食材ID',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='レシピ_食材';
+-- テーブルのデータのダンプ `recipe_ingredient`
+INSERT INTO `recipe_ingredient` (`id`, `recipe_id`, `ingredient_id`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, 1, 1, '2024-01-15 10:20:25', '2024-01-15 10:20:25', NULL, 0),
+(2, 1, 2, '2024-01-15 10:21:11', '2024-01-15 10:21:11', NULL, 0),
+(3, 2, 3, '2024-01-15 10:22:05', '2024-01-15 10:22:05', NULL, 0);
 -- テーブルのインデックス `recipe_ingredient`
 ALTER TABLE `recipe_ingredient`
   ADD PRIMARY KEY (`id`),
@@ -258,15 +266,12 @@ ALTER TABLE `recipe_ingredient`
   ADD KEY `ingredient_id` (`ingredient_id`);
 -- テーブルの AUTO_INCREMENT `recipe_ingredient`
 ALTER TABLE `recipe_ingredient`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=4;
 -- テーブルの制約 `recipe_ingredient`
 ALTER TABLE `recipe_ingredient`
   ADD CONSTRAINT `recipe_ingredient_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`),
   ADD CONSTRAINT `recipe_ingredient_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `foodstuff` (`id`);
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `taste`
@@ -275,19 +280,21 @@ CREATE TABLE `taste` (
   `taste_name` varchar(256) NOT NULL COMMENT '系統名',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='系統';
+-- テーブルのデータのダンプ `taste`
+INSERT INTO `taste` (`id`, `taste_name`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, 'さっぱり系', '2024-01-11 11:44:52', '2024-01-11 11:44:52', NULL, 0),
+(2, 'がっつり系', '2024-01-11 11:44:52', '2024-01-11 11:44:52', NULL, 0),
+(3, '辛い系', '2024-01-11 11:44:52', '2024-01-11 11:44:52', NULL, 0);
 -- テーブルのインデックス `taste`
 ALTER TABLE `taste`
   ADD PRIMARY KEY (`id`);
 -- テーブルの AUTO_INCREMENT `taste`
 ALTER TABLE `taste`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=4;
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `recipe_taste`
@@ -297,9 +304,13 @@ CREATE TABLE `recipe_taste` (
   `taste_id` bigint(20) NOT NULL COMMENT '系統ID',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
-  `delete_falg` tinyint(4) NOT NULL COMMENT '削除フラグ'
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
+  `delete_flag` tinyint(4) NOT NULL COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='レシピ_系統';
+-- テーブルのデータのダンプ `recipe_taste`
+INSERT INTO `recipe_taste` (`id`, `recipe_id`, `taste_id`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, 1, 1, '2024-01-15 10:24:33', '2024-01-15 10:24:33', NULL, 0),
+(2, 2, 1, '2024-01-15 10:25:01', '2024-01-15 10:25:01', NULL, 0);
 -- テーブルのインデックス `recipe_taste`
 ALTER TABLE `recipe_taste`
   ADD PRIMARY KEY (`id`),
@@ -307,14 +318,12 @@ ALTER TABLE `recipe_taste`
   ADD KEY `taste_id` (`taste_id`);
 -- テーブルの AUTO_INCREMENT `recipe_taste`
 ALTER TABLE `recipe_taste`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=3;
 -- テーブルの制約 `recipe_taste`
 ALTER TABLE `recipe_taste`
   ADD CONSTRAINT `recipe_taste_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`),
   ADD CONSTRAINT `recipe_taste_ibfk_2` FOREIGN KEY (`taste_id`) REFERENCES `taste` (`id`);
 COMMIT;
-
-
 
 
 -- テーブルの構造 `genre`
@@ -323,19 +332,22 @@ CREATE TABLE `genre` (
   `genre_name` varchar(256) NOT NULL COMMENT 'ジャンル名',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='ジャンル';
+-- テーブルのデータのダンプ `genre`
+INSERT INTO `genre` (`id`, `genre_name`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, '和風料理', '2024-01-11 11:51:40', '2024-01-11 11:51:40', NULL, 0),
+(2, '中華料理', '2024-01-11 11:51:40', '2024-01-11 11:51:40', NULL, 0),
+(3, '洋風料理', '2024-01-11 11:51:40', '2024-01-11 11:51:40', NULL, 0);
 -- テーブルのインデックス `genre`
 ALTER TABLE `genre`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 -- テーブルの AUTO_INCREMENT `genre`
 ALTER TABLE `genre`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=4;
 COMMIT;
-
-
 
 
 -- テーブルの構造 `recipe_genre`
@@ -345,9 +357,13 @@ CREATE TABLE `recipe_genre` (
   `genre_id` bigint(20) NOT NULL COMMENT 'ジャンルID',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='レシピ_ジャンル';
+-- テーブルのデータのダンプ `recipe_genre`
+INSERT INTO `recipe_genre` (`id`, `recipe_id`, `genre_id`, `create_date`, `update_date`, `delete_date`, `delete_flag`) VALUES
+(1, 1, 1, '2024-01-15 10:16:04', '2024-01-15 10:16:04', NULL, 0),
+(2, 2, 1, '2024-01-15 10:16:45', '2024-01-15 10:16:45', NULL, 0);
 -- テーブルのインデックス `recipe_genre`
 ALTER TABLE `recipe_genre`
   ADD PRIMARY KEY (`id`),
@@ -355,15 +371,12 @@ ALTER TABLE `recipe_genre`
   ADD KEY `genre_id` (`genre_id`);
 -- テーブルの AUTO_INCREMENT `recipe_genre`
 ALTER TABLE `recipe_genre`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=3;
 -- テーブルの制約 `recipe_genre`
 ALTER TABLE `recipe_genre`
   ADD CONSTRAINT `recipe_genre_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`),
   ADD CONSTRAINT `recipe_genre_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`);
 COMMIT;
-
-
-
 
 
 -- テーブルの構造 `seasoning`
@@ -372,7 +385,7 @@ CREATE TABLE `seasoning` (
   `seasoning_name` varchar(256) NOT NULL COMMENT '調味料名',
   `create_date` datetime NOT NULL DEFAULT current_timestamp() COMMENT '作成日時',
   `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
-  `delete_date` datetime COMMENT '削除日時',
+  `delete_date` datetime DEFAULT NULL COMMENT '削除日時',
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '削除フラグ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='調味料';
 -- テーブルのインデックス `seasoning`
@@ -383,10 +396,6 @@ ALTER TABLE `seasoning`
 ALTER TABLE `seasoning`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id';
 COMMIT;
-
-
-
-
 
 
 -- テーブルの構造 `recipe_seasoning`
@@ -412,10 +421,6 @@ ALTER TABLE `recipe_seasoning`
   ADD CONSTRAINT `recipe_seasoning_ibfk_1` FOREIGN KEY (`recipi_id`) REFERENCES `recipe` (`id`),
   ADD CONSTRAINT `recipe_seasoning_ibfk_2` FOREIGN KEY (`seasoning_id`) REFERENCES `seasoning` (`id`);
 COMMIT;
-
-
-
-
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
