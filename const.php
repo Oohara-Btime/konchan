baecd866deeda053cabd6639906bf0f80e2d88ec
hiyamin <kkr2290007@stu.o-hara.ac.jp>
2024-01-16 11:11:27 +0900

    競合の解消

--- const.php
+++ const.php
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-<?php
-define("DSN",   "mysql:dbname=konchan;host=localhost");
-define("DB_USER",   "root");
-=======
 <?php
 // 有効な定数名
 define("DSN",     "mysql:dbname=konchan;host=localhost");
 define("DB_USER",    "root");
->>>>>>> 6e46c9389ce5d98fa9760b99c42410195eafbfb7
 ?>
