941305b1d4e982a41ced49ae06a4e86668c99d96
amiki ryohei <kkr2290071@stu.o-hara.ac.jp>
2024-01-17 15:36:11 +0900

    郷土料理の検索の完成

--- subscription/unsubscribe_process.php
+++ subscription/unsubscribe_process.php
@@ -1,8 +1,12 @@
 
 <?php
 include("../const.php");
+session_start();
+
+
+
 if (isset($_SESSION['email'])) {
-    $sql=$pdo->prepare('update user set delete_flag=true');
+    $sql=$pdo->prepare('update user set delete_flag=1');
     $sql->execute([$_REQUEST['delete_flag']]);
     header("Location:../index.php");
     exit();
