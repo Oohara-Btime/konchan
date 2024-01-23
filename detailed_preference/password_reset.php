2218ace9c7574b4fcb95b70152b34b1b2ce4371e
amiki ryohei <kkr2290071@stu.o-hara.ac.jp>
2024-01-23 10:14:25 +0900

    パスワードリセットの変更

--- detailed_preference/password_reset.php
+++ detailed_preference/password_reset.php
@@ -9,28 +9,36 @@ if ($error == 1) {
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="../css/setting.css">
-    </head>
 
-    <body>
-        <h1>パスワードリセット</h1>
-        <div class="form">
-            <form action="password_resetting.php" method="post">
-                <div class="text-input">
-                    <label for="username">メールアドレス入力</label>
-                    <input type="text" name="username" id="username" placeholder="" />
-                    <span class="separator"> </span>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/setting.css">
+</head>
 
-                <div class="form-bottom">
-                    <input type="submit" id="submit" value="完了" />
-                </div>
-            </form>
-        </div>
-        <script src="../js/setting.js"></script>
-    </body>
+<body>
+    <h1>パスワードリセット</h1>
+    <div class="form">
+        <form action="password_resetting.php" method="post">
+            <div class="text-input">
+                <?php
+                if ($error == 1) {
+                    echo "<FONT COLOR=RED>$errormsg</FONT><br/>";
+                }
+                ?>
+                <label for="email">メールアドレス入力</label>
+                <input type="text" name="email" id="email" placeholder="" />
+                <span class="separator"> </span>
+            </div>
+
+            <div class="form-bottom">
+                <!-- <input type="submit" id="submit" value="完了"/> -->
+                <button type="submit" id="submit">送信</button>
+            </div>
+        </form>
+    </div>
+    <script src="../js/setting.js"></script>
+</body>
+
 </html>
