22097f40363fb790df0806edf1c0343c4d51bc66
amiki ryohei <kkr2290071@stu.o-hara.ac.jp>
2024-01-24 14:10:34 +0900

    サブスクのバック

--- detailed_preference/subscription_registration.php
+++ detailed_preference/subscription_registration.php
@@ -9,15 +9,16 @@
 </head>
 
 <body>
-    <h1>有料会員登録</h1>
-    <h2>広告が非表示に！</h2>
-    <p>出てくる広告が非表示！ストレスなく快適にご利用できます。</p>
-    <div class="container">
-        <div class="radio-tile-group">
-            <div class="input-container">
-                <input id="walk" class="radio-button" type="radio" name="radio" />
-                <div class="radio-tile">
-                    <!-- <div class="icon walk-icon">
+    <form action="subscription_process.php" method="post">
+        <h1>有料会員登録</h1>
+        <h2>広告が非表示に！</h2>
+        <p>出てくる広告が非表示！ストレスなく快適にご利用できます。</p>
+        <div class="container">
+            <div class="radio-tile-group">
+                <div class="input-container">
+                    <input id="walk" class="radio-button" type="radio" name="radio" value="１か月" />
+                    <div class="radio-tile">
+                        <!-- <div class="icon walk-icon">
                         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 0h24v24H0z" fill="none" />
@@ -25,19 +26,19 @@
                                 d="M13.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM9.8 8.9L7 23h2.1l1.8-8 2.1 2v6h2v-7.5l-2.1-2 .6-3C14.8 12 16.8 13 19 13v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1-.3 0-.5.1-.8.1L6 8.3V13h2V9.6l1.8-.7" />
                         </svg>
                     </div> -->
-                    <label for="fly" class="radio-tile-label">お試しにいかがですか？</label>
-                    <label for="fly" class="number-tile-label">1</label>
-                    <label for="fly" class="months-tile-label">カ月</label>
-                    <label for="fly" class="transparency-tile-label">透明</label>
-                    <hr>
-                    <label for="fly" class="price-tile-label">￥100</label>
+                        <label for="fly" class="radio-tile-label">お試しにいかがですか？</label>
+                        <label for="fly" class="number-tile-label">1</label>
+                        <label for="fly" class="months-tile-label">カ月</label>
+                        <label for="fly" class="transparency-tile-label">透明</label>
+                        <hr>
+                        <label for="fly" class="price-tile-label">￥100</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="input-container">
-                <input id="bike" class="radio-button" type="radio" name="radio" />
-                <div class="radio-tile">
-                    <!-- <div class="icon bike-icon">
+                <div class="input-container">
+                    <input id="bike" class="radio-button" type="radio" name="radio" value="３か月"/>
+                    <div class="radio-tile">
+                        <!-- <div class="icon bike-icon">
                         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 0h24v24H0z" fill="none" />
@@ -45,19 +46,19 @@
                                 d="M15.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM5 12c-2.8 0-5 2.2-5 5s2.2 5 5 5 5-2.2 5-5-2.2-5-5-5zm0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5 3.5 1.6 3.5 3.5-1.6 3.5-3.5 3.5zm5.8-10l2.4-2.4.8.8c1.3 1.3 3 2.1 5.1 2.1V9c-1.5 0-2.7-.6-3.6-1.5l-1.9-1.9c-.5-.4-1-.6-1.6-.6s-1.1.2-1.4.6L7.8 8.4c-.4.4-.6.9-.6 1.4 0 .6.2 1.1.6 1.4L11 14v5h2v-6.2l-2.2-2.3zM19 12c-2.8 0-5 2.2-5 5s2.2 5 5 5 5-2.2 5-5-2.2-5-5-5zm0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5 3.5 1.6 3.5 3.5-1.6 3.5-3.5 3.5z" />
                         </svg>
                     </div> -->
-                    <label for="fly" class="radio-tile-label">1番人気!</label>
-                    <label for="fly" class="number-tile-label">3</label>
-                    <label for="fly" class="months-tile-label">カ月</label>
-                    <label for="fly" class="transparency-tile-label">透明</label>
-                    <hr>
-                    <label for="fly" class="price-tile-label">￥300</label>
+                        <label for="fly" class="radio-tile-label">1番人気!</label>
+                        <label for="fly" class="number-tile-label">3</label>
+                        <label for="fly" class="months-tile-label">カ月</label>
+                        <label for="fly" class="transparency-tile-label">透明</label>
+                        <hr>
+                        <label for="fly" class="price-tile-label">￥300</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="input-container">
-                <input id="drive" class="radio-button" type="radio" name="radio" />
-                <div class="radio-tile">
-                    <!-- <div class="icon car-icon">
+                <div class="input-container">
+                    <input id="drive" class="radio-button" type="radio" name="radio" value="６か月"/>
+                    <div class="radio-tile">
+                        <!-- <div class="icon car-icon">
                         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path
@@ -65,19 +66,19 @@
                             <path d="M0 0h24v24H0z" fill="none" />
                         </svg>
                     </div> -->
-                    <label for="fly" class="radio-tile-label">1番おすすめ!</label>
-                    <label for="fly" class="number-tile-label">6</label>
-                    <label for="fly" class="months-tile-label">カ月</label>
-                    <label for="fly" class="transparency-tile-label">透明</label>
-                    <hr>
-                    <label for="fly" class="price-tile-label">￥600</label>
+                        <label for="fly" class="radio-tile-label">1番おすすめ!</label>
+                        <label for="fly" class="number-tile-label">6</label>
+                        <label for="fly" class="months-tile-label">カ月</label>
+                        <label for="fly" class="transparency-tile-label">透明</label>
+                        <hr>
+                        <label for="fly" class="price-tile-label">￥600</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="input-container">
-                <input id="fly" class="radio-button" type="radio" name="radio" />
-                <div class="radio-tile">
-                    <!-- <div class="icon fly-icon">
+                <div class="input-container">
+                    <input id="fly" class="radio-button" type="radio" name="radio" value="１２か月"/>
+                    <div class="radio-tile">
+                        <!-- <div class="icon fly-icon">
                         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.18 9" />
@@ -86,24 +87,26 @@
                             <path d="M0 0h24v24H0z" fill="none" />
                         </svg>
                     </div> -->
-                    <label for="fly" class="radio-tile-label">1番お得!</label>
-                    <label for="fly" class="number-tile-label">12</label>
-                    <label for="fly" class="months-tile-label">カ月</label>
-                    <label for="fly" class="discount-tile-label">18%割引</label>
-                    <hr>
-                    <label for="fly" class="price-tile-label">￥1,000</label>
-                    
+                        <label for="fly" class="radio-tile-label">1番お得!</label>
+                        <label for="fly" class="number-tile-label">12</label>
+                        <label for="fly" class="months-tile-label">カ月</label>
+                        <label for="fly" class="discount-tile-label">18%割引</label>
+                        <hr>
+                        <label for="fly" class="price-tile-label">￥1,000</label>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="button">
-        <button class="registration">登録する</button><br>
-        <button class="cancel">キャンセル</button>
-    </div>
+        <div class="button">
+            <button type="submit" name="sumbit" id="submit" class="registration">登録する</button><br>
+            <button type="button" onclick="location.href='../index.php'" class="cancel">キャンセル</button>
+        </div>
 
-    <script src="menu_bar.js"></script> 
+        <script src="menu_bar.js"></script>
+
+    </form>
 </body>
 
 </html>
